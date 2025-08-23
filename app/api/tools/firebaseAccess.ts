8d3b64a65830735b148551226c95dc26914164af
123654-dev <72887184+123654-dev@users.noreply.github.com>
2025-08-23 20:10:19 +0200

    groovy text

--- app/api/tools/firebaseAccess.ts
+++ app/api/tools/firebaseAccess.ts
@@ -11,20 +11,23 @@ import { collection, getDocs } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyBm9qW8JokMPQ65sf6YlJCh7AA89MnK-eU",
   authDomain: "montypytorchxpostfinance.firebaseapp.com",
-  databaseURL: "https://montypytorchxpostfinance-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://montypytorchxpostfinance-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "montypytorchxpostfinance",
   storageBucket: "montypytorchxpostfinance.firebasestorage.app",
   messagingSenderId: "841032630437",
   appId: "1:841032630437:web:c14d8cc05f0308bb098d4c",
-  measurementId: "G-9GGWB051BB"
+  measurementId: "G-9GGWB051BB",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-//use firestore to get the value for the first entry key of userSummaries
-const db = getFirestore(app);
-const userSummariesRef = collection(db, "userSummaries");
-const firstUserSummary = await getDocs(userSummariesRef);
-const firstUserSummaryData = firstUserSummary.docs[0].data();
-
+export async function getUserSummary() {
+  //use firestore to get the value for the first entry key of userSummaries
+  const db = getFirestore(app);
+  const userSummariesRef = collection(db, "userSummaries");
+  const firstUserSummary = await getDocs(userSummariesRef);
+  const firstUserSummaryData = firstUserSummary.docs[0].data();
+  return firstUserSummaryData;
+}
